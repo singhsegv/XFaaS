9cb0ea80f20fc634873dcb309b59d39496bb9b1e
Tarun Pal <tanmaypal13982@gmail.com>
2024-06-14 17:17:41 +0530

    Added independent sqs support for aws and seprated deployment as per csp

--- serwo/templates/azure/predefined-functions/CollectLogs/func.py
+++ serwo/templates/azure/predefined-functions/CollectLogs/func.py
@@ -3,14 +3,20 @@ import json
 from python.src.utils.classes.commons.serwo_objects import SerWOObject
 import os, uuid
 import logging
-
-connect_str = 'CONNECTION_STRING'
-queue_name = 'QUEUE_NAME'
-
-queue = QueueClient.from_connection_string(conn_str=connect_str, queue_name=queue_name)
-
-
-
+import boto3
+ 
+connect_str = "CONNECTION_STRING"
+queue_name = "QUEUE_NAME"
+csp = "COLL_CSP"
+ 
+if csp == "aws":
+    queue = boto3.client("sqs")
+elif csp == "azure":
+    queue = QueueClient.from_connection_string(
+        conn_str=connect_str, queue_name=queue_name
+    )
+ 
+ 
 def user_function(serwoObject) -> SerWOObject:
     try:
         fin_dict = dict()
@@ -20,8 +26,12 @@ def user_function(serwoObject) -> SerWOObject:
         fin_dict["data"] = data
         fin_dict["metadata"] = metadata
         logging.info("Fin dict - "+str(fin_dict))
-        queue.send_message(json.dumps(fin_dict))
+        if csp == "azure":
+            queue.send_message(json.dumps(fin_dict))
+        elif csp == "aws":
+            queue.send_message(MessageBody=json.dumps(fin_dict), QueueUrl=connect_str)
         # data = {"body": "success: OK"}
         return SerWOObject(body=serwoObject.get_body())
     except Exception as e:
         return SerWOObject(error=True)
+ 
