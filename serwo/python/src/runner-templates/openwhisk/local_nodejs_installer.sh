52faeb656ccf8ffca88066ffb3ec3abc57506b47
rajdeep1008 <iamrajdeep1008@gmail.com>
2024-06-24 15:41:50 +0530

    local nodejs version changes for binary

--- serwo/python/src/runner-templates/openwhisk/local_nodejs_installer.sh
+++ serwo/python/src/runner-templates/openwhisk/local_nodejs_installer.sh
@@ -1,4 +1,4 @@
 NODEJS_DIR=$1
 
 export N_PREFIX=$NODEJS_DIR
-curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s lts
+curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s 14.20.0
